5be59d9ad7ea6f251be483fb4f7dbe1005303e5b
Supimpa <afonso2011fachineti@hotmail.com>
2022-06-14 20:33:14 -0300

    Configure heroku

---
+++ book/book/settings/heroku.py
@@ -0,0 +1,15 @@
+import environ
+
+from book.book.settings.base import *
+
+env = environ.ENV()
+
+DEBUG = env.bool("DEBUG", False)
+
+SECRETY_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+DATABASES = {
+    "default": env.db()
+}
