eabb53c736e3827d53200363a9337305be8ee9fc
Supimpa <afonso2011fachineti@hotmail.com>
2022-06-15 14:16:16 -0300

    Create serealizers from models books

--- book/books/models.py
+++ book/books/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 
 
+class Author:
+    author = models.CharField(max_length=150, unique=True, db_index=models.CASCADE)
+
+
+
+class Genero:
+    genero = models.CharField(max_length=100, unique=True, db_index=models.CASCADE)
+
+
 class Books:
     titulo = models.CharField(max_length=100)
-    author = models.ForeignKey
+    author = models.ForeignKey(Author)
     ano = models.IntegerField(max_length=4)
-    genero = models.ForeignKey
+    genero = models.ForeignKey(Genero)
     price = models.DecimalField(max_length=6, decimal_places=2)
     visible = models.BooleanField(default=True)
 
     def __str__(self):
         return self.titulo
-
-
-class Author:
-    author = models.CharField(max_length=150, unique=True, db_index=models.CASCADE)
-
-
-class Genero:
-    genero = models.CharField(max_length=100, unique=True, db_index=models.CASCADE)
